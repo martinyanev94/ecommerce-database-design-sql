fe8f72013cd0793019a691fb6d034fd11fd5ef32
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-11-23 07:18:20 -0500

    Add Lesson_1_Understanding_Databases_And_Data_Models/part_5_populate_books_and_categories.sql

---
+++ Lesson_1_Understanding_Databases_And_Data_Models/part_5_populate_books_and_categories.sql
@@ -0,0 +1,9 @@
+INSERT INTO categories (category_name) VALUES
+('Fiction'),
+('Science Fiction'),
+('Non-Fiction');
+
+INSERT INTO books (title, author, published_year, category_id) VALUES
+('Dune', 'Frank Herbert', 1965, 2),
+('1984', 'George Orwell', 1949, 1),
+('Sapiens', 'Yuval Noah Harari', 2011, 3);
